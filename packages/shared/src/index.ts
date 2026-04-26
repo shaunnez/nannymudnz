07cc464341502748c20f7a254cf5be9aff8b16bc
Nannymud Dev <dev@nannymud.local>
2026-04-26 14:13:57 +1200

    chore: export new pickup types from shared package index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from './schema';
 export * from './protocol/messages';
+export type { PickupType, PickupCategory, PickupDef } from './simulation/pickupData';
+export { PICKUP_DEFS, CRATE_LOOT_TABLE, STAGE_CRATES } from './simulation/pickupData';
